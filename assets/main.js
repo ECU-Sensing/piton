9d82910137c29b86203fa787092e3251331e6f47
ColbySawyer7 <sawyerc17@students.ecu.edu>
2023-08-23 16:43:59 -0400

    Added Popup Fixed, Mobile Styling for Package Cards

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,57 @@
+function showPackagesPopup() {
+    if (window.innerWidth > 600) {
+        var popup = document.querySelector('.packages-popup');
+        popup.classList.add('visible');
+        document.body.classList.add('popup-active');
+    }
+}
+
+
+function closePackagesPopup() {
+    var popup = document.querySelector('.packages-popup');
+    popup.classList.remove('visible');
+    document.body.classList.remove('popup-active');
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    //console.log("DOMContentLoaded event triggered!");  // This will log when the DOM is loaded
+    // Add click events to each packages-card to show the popup
+    var cards = document.querySelectorAll('.packages-card');
+    cards.forEach(function(card) {
+        card.addEventListener('click', function(event) {
+            // Stop the event from propagating to the document level
+            event.stopPropagation();
+            
+            // Toggle the popup
+            var popup = document.querySelector('.packages-popup');
+                if (popup.classList.contains('visible')) {
+                    closePackagesPopup();
+                } else {
+                    showPackagesPopup();
+                }
+
+        });
+    });
+    
+});
+
+document.addEventListener('click', function(event) {
+    var popup = document.querySelector('.packages-popup');
+    
+    // Check if the popup is visible
+    if (popup.classList.contains('visible')) {
+        
+        // If the click target is not inside the popup, close the popup
+        if (!popup.contains(event.target)) {
+            closePackagesPopup();
+        }
+    }
+});
+
+
+
+
 function toggleMenu(displayState) {
     const nav = document.getElementById('header_navigation');
     const body = document.body;
@@ -27,4 +81,3 @@ function navigateAndCloseMenu(linkElement) {
         targetElement.scrollIntoView();
     }
 }
-
